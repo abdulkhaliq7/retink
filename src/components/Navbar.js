28849bf0a1c6ab6a7d1cfced09e88e731a764e60
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-12-08 11:17:40 +0300

    added navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,21 +8,21 @@ function Navbar() {
     const closeMobileMenu = () => setClick(false)
     const [navbar, setNavbar] = useState(false)
 
-    const changeBackground = () => {
-        if (window.scrollY >= 79 ) {
-            setNavbar(true)
-        } else {
-            setNavbar(false)
-        }
-    }
+    // const changeBackground = () => {
+    //     if (window.scrollY >= 79 ) {
+    //         setNavbar(true)
+    //     } else {
+    //         setNavbar(false)
+    //     }
+    // }
 
-    window.addEventListener('scroll', changeBackground)
+    // window.addEventListener('scroll', changeBackground)
 
     return (
         <>
             <nav className={navbar ? 'navbar active' : 'navbar'}>
                 <Link to='/' className='navbar-logo'>
-                    Poverty Interventions
+                    Poverty
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -37,27 +37,30 @@ function Navbar() {
                     <li className='nav-item'>
                         <Link to='/who-we-are' className='nav-links' onClick=
                         {closeMobileMenu}> 
-                            Who we are
+                            Services
                         </Link>
                     </li>
                     <li className='nav-item'>
                         <Link to='/interventions' className='nav-links' onClick=
                         {closeMobileMenu}> 
-                            Interventions
+                            About us
                         </Link>
                     </li>
-                    <li className='nav-item'>
-                        <Link to='/contact-us' className='nav-links' onClick=
-                        {closeMobileMenu}> 
-                            Contact Us
-                        </Link>
-                    </li>
+                    
                     {/* <li className='nav-item'>
                         <Link to='/resources' className='nav-links' onClick=
                         {closeMobileMenu}> 
                             Resources
                         </Link>
                     </li> */}
+                </ul>
+                <ul>
+                    <li className='nav-item login' >
+                        <Link to='/contact-us' className='nav-links' onClick=
+                        {closeMobileMenu}> 
+                            Login
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </>
